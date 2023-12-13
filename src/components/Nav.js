a00852c681abc3fb30faf9f0de16b6a606e9953f
mihye0924 <mihye0924@naver.com>
2023-12-13 16:51:23 +0900

    update deploy!

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -102,10 +102,11 @@ export default Nav
 
 const DropDown = styled.div`
   position: absolute;
-  top:48px;
-  right: 0;
-  background: rgb(19, 19, 19)
-  border: 1px solid rgba(151, 151, 151, 0.34)
+  top: 60px;
+  right: 0px;
+  text-align: center;
+  // background: rgb(19, 19, 19);
+  // border: 1px solid rgba(151, 151, 151, 0.34);
   border-radius: 4px;
   box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
   padding: 10px; 
